54b4d6e6514f35a1245878e39193b4bcbe63c353
Chhingchhing HEM <chhingchhing.hem@gmail.com>
2015-10-08 23:14:15 +0700

    Amount in word + style pagination

--- application/controllers/loans.php
+++ application/controllers/loans.php
@@ -7,6 +7,43 @@ class Loans extends MAIN_Controller {
 		$this->clear_sessions();
 		$data['controller_name'] = strtolower(get_class());
 
+    /* This Application Must Be Used With BootStrap 3 *  */
+    /*$config['full_tag_open'] = "<ul class='pagination'>";
+    $config['full_tag_close'] ="</ul>";
+    $config['num_tag_open'] = '<li>';
+    $config['num_tag_close'] = '</li>';
+    $config['cur_tag_open'] = "<li class='disabled'><li class='active'><a href='#'>";
+    $config['cur_tag_close'] = "<span class='sr-only'></span></a></li>";
+    $config['next_tag_open'] = "<li>";
+    $config['next_tagl_close'] = "</li>";
+    $config['prev_tag_open'] = "<li>";
+    $config['prev_tagl_close'] = "</li>";
+    $config['first_tag_open'] = "<li>";
+    $config['first_tagl_close'] = "</li>";
+    $config['last_tag_open'] = "<li>";
+    $config['last_tagl_close'] = "</li>";*/
+
+     //config for bootstrap pagination class integration
+    $config['full_tag_open'] = '<ul class="pagination">';
+    $config['full_tag_close'] = '</ul>';
+    $config['first_link'] = false;
+    $config['last_link'] = false;
+    $config['first_tag_open'] = '<li>';
+    $config['first_tag_close'] = '</li>';
+    $config['prev_link'] = '&laquo';
+    $config['prev_tag_open'] = '<li class="prev">';
+    $config['prev_tag_close'] = '</li>';
+    $config['next_link'] = '&raquo';
+    $config['next_tag_open'] = '<li>';
+    $config['next_tag_close'] = '</li>';
+    $config['last_tag_open'] = '<li>';
+    $config['last_tag_close'] = '</li>';
+    $config['cur_tag_open'] = '<li class="active"><a href="#">';
+    $config['cur_tag_close'] = '</a></li>';
+    $config['num_tag_open'] = '<li>';
+    $config['num_tag_close'] = '</li>';
+
+
     $config['base_url'] = site_url('loans/index');
     $config['per_page'] = 2; 
     $config['total_rows'] = $this->Loan_approval->count_all();
